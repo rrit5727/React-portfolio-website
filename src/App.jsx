556bc4d549d40cd5211b399fdfb6613defaf370a
rrit5727 <rrit5727@gmail.com>
2024-05-20 21:22:08 +1000

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -6,7 +7,9 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <h1>Hello world</h1>
+      </div>
     </>
   )
 }
