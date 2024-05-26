4dea4a6b236d0fbd601bf7fce2fcb2688892b02b
rrit5727 <rrit5727@gmail.com>
2024-05-26 20:49:44 +1000

    resolve type animation layout shift|

--- src/App.jsx
+++ src/App.jsx
@@ -218,7 +218,7 @@ export default function App() {
                               ]}
                               wrapper="span"
                               speed={{ type: 'keyStrokeDelayInMs', value: 30 }}
-                              style={{  display: 'inline-block' }}
+                              style={{  display: 'inline-block', width: '250px' }}
                               class="custom-type-animation-cursor"
                               repeat={0}
                             />
